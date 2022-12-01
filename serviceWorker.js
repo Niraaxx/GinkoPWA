a0c5528945f6454e9eb75b61e6b3952ed57d009f
Tanguy <tanguycrussard@gmail.com>
2022-12-01 15:21:22 +0100

    Add serviceWorker

---
+++ serviceWorker.js
@@ -0,0 +1,43 @@
+const cacheName = 'ginkoCache';
+const appShellFiles = [
+    'index.html',
+    'app.js',
+    'style.css',
+    'manifest.json',
+    'icons/favicon.ico',
+    'icons/icon-32.png',
+    'icons/icon-64.png',
+    'icons/icon-512.png',
+    'icons/maskable_icon.png'
+  ];
+
+  self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(appShellFiles);
+    })());
+  });
+
+  self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
+
+  self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key === cacheName) { return; }
+        return caches.delete(key);
+      }));
+    }));
+  });
